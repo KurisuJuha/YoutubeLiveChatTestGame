d856473d454e380ad61475c0cdc69fc5124ff914
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-28 01:26:24 +0900

    Cshchatの導入

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KYapp.Cshchat;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +11,13 @@ public class GameManager : MonoBehaviour
     public GameObject GamePanel;
 
     public bool IsStarted;
+
+    public TMP_InputField liveIdInput;
+    public TMP_InputField WordInput;
+
+    public string liveID;
+
+    YoutubeLive youtubeLive;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +37,18 @@ public class GameManager : MonoBehaviour
     {
         TitlePanel.SetActive(false);
         GamePanel.SetActive(true);
+
+        youtubeLive = new YoutubeLive(liveIdInput.text);
+        youtubeLive.Begin(OnComment);
+    }
+
+    public void OnComment(Comment com)
+    {
+        Debug.Log(com.text);
+    }
+
+    public void OnDestroy()
+    {
+        youtubeLive.End();
     }
 }
