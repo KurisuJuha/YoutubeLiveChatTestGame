15677dae8ec9705f5f6c5b1d913c3754a0b27612
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-28 21:47:39 +0900

    system.text.jsonをセットアップした

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using KYapp.Cshchat;
+using System.Text.RegularExpressions;
 using TMPro;
 
 
@@ -18,10 +19,14 @@ public class GameManager : MonoBehaviour
     public string liveID;
 
     YoutubeLive youtubeLive;
+
+    public GameObject WordPrefab;
+    public GameObject Words;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TitlePanel.SetActive(true);
+        GamePanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -45,6 +50,14 @@ public class GameManager : MonoBehaviour
     public void OnComment(Comment com)
     {
         Debug.Log(com.text);
+        Match a = Regex.Match(com.text, @"‰¹—Ê\(0\)(.+?)");
+        if (a.Success)
+        {
+            Debug.Log("aaaaaa");
+            GameObject go = Instantiate(WordPrefab).transform.GetChild(0).gameObject;
+            //TMP_Text text = go.GetComponent<TMP_Text>();
+            //text.text = a.Groups[1].Value;
+        }
     }
 
     public void OnDestroy()
